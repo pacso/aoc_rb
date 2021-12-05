7a339ee250fa85d749b39cf231410a415b164a5a
Jon Pascoe <jon.pascoe@me.com>
2021-12-05 14:58:13 +0000

    Bump version

--- spec/lib/aoc_rb_spec.rb
+++ spec/lib/aoc_rb_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 RSpec.describe AocRb do
   it "has the expected version number" do
-    expect(AocRb::VERSION).to eq "0.2.0"
+    expect(AocRb::VERSION).to eq "0.2.1"
   end
 end
